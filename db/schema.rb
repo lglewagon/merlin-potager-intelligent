9eca9b2fe3c6ef64b1cb3a357716536444dc963b
Louis Gesbert <louis.gesbert@hotmail.com>
2021-10-12 17:56:37 +0200

    liaison entre task et plant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_12_155259) do
+ActiveRecord::Schema.define(version: 2021_10_12_155535) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,8 @@ ActiveRecord::Schema.define(version: 2021_10_12_155259) do
     t.text "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "plants_id"
+    t.index ["plants_id"], name: "index_tasks_on_plants_id"
   end
 
   create_table "users", force: :cascade do |t|
